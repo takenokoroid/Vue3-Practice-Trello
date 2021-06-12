9512063e3239a8f91c7ccd0e651d315e043bf161
takenokoroid <takenokoroid@gmail.com>
2021-06-12 06:43:35 +0000

    resolve #1

---
+++ src/trello/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul class="todoList">
+      <li v-for="todo in state.todoList" :key="todo.todo">{{ todo.todo }}</li>
+    </ul>
+    <todo-input></todo-input>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive } from "vue";
+import TodoInput from "./TodoInput.vue";
+export default defineComponent({
+  name: "TodoList",
+  components: { TodoInput },
+
+  setup() {
+    const state = reactive({
+      todoList: [{ todo: "todo1" }, { todo: "todo2" }],
+    });
+    return { state };
+  },
+});
+</script>
+
+<style></style>
